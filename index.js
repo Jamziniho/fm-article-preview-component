4cf855dd48e187cddf10341e688e9176927d15ea
Jamal Abubakari <jamalabubalari@gmail.com>
2024-09-15 13:47:19 +0100

    - Sunday 15th September 2024

---
+++ index.js
@@ -0,0 +1,38 @@
+
+function toggle() {
+  const shareButton = document.getElementById("share-button")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(77667);
+
+
+const shareButton = document.getElementById("share-button");
+const openShare = document.getElementsByClassName("open-share");
+
+shareButton.addEventListener("click", event => {
+
+if(openShare[0].style.display === "none"){
+  openShare[0].style.display = "block";
+}
+else{
+  openShare[0].style.display = "none";
+}
+
+});
